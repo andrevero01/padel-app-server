4c6cf12c1b26259c58e53e67495853cff34470d9
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-07-18 18:40:07 +0200

    Test

--- models/Team.model.js
+++ models/Team.model.js
@@ -14,10 +14,12 @@ const teamSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "Player",
     },
-    homeCourt: {
-      type: Schema.Types.ObjectId,
-      ref: "Court",
-    },
+    courts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Court",
+      },
+    ],
     leagues: [
       {
         type: Schema.Types.ObjectId,
