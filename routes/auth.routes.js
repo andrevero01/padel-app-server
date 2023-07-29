4b3289c370b6eb3e08378dbabd875b82ed1aeb86
Andrea <andre.mora89@gmail.com>
2023-07-29 12:56:40 +0200

    main merge

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -44,11 +44,7 @@ router.post("/login", (req, res, next) => {
 
 // GET - verify JWT stored on the client
 router.get("/verify", isAuthenticated, (req, res, next) => {
-  // If JWT token is valid the payload gets decoded by the
-  // isAuthenticated middleware and made available on `req.payload`
   console.log(`req.payload`, req.payload);
-  // Send back the object with user data
-  // previously set as the token payload
   res.status(200).json(req.payload);
 });
 
